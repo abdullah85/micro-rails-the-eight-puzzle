4f8c4fd324aa543ec55e65b0815564392a3fc60b
Abdullah A.K <4144429+abdullah85@users.noreply.github.com>
2024-02-01 08:29:58 +0530

    generated controller, added index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root "eight_puzzles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :eight_puzzles do
+    member do
+      get :delete
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
